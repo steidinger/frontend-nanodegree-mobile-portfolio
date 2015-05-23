36f1fff58875100b29aee6386866cfa3f37f9065
Frank Steidinger <steidinger@acm.org>
2015-05-23 17:23:33 +0200

    use grunt-contrib-connect to serve html files
    Provide two versions of the html server: one for development, using the source files and one for "production", using the minified files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,12 +31,33 @@ module.exports = function(grunt) {
                     'dist/index.html' : ['index.html']
                 }
             }
+        },
+        connect: {
+            dev: {
+                options: {
+                    port: 8080,
+                    keepalive: true
+                }
+            },
+            prod: {
+                options: {
+                    port: 8080,
+                    keepalive: true,
+                    base: {
+                        path: 'dist',
+                        options: {
+                            maxAge: 365 * 24 * 60 * 60 * 1000 // 1 year
+                        }
+                    }
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
     // Default task(s).
     grunt.registerTask('default', ['imagemin', 'cssmin', 'htmlmin']);
